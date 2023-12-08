8fb7d0c17696f4fd974e28c953aa124283992b3d
Rishabhkhanna22 <147158567+Rishabhkhanna22@users.noreply.github.com>
2023-12-08 04:20:45 +0000

    Update primes.py

--- primes.py
+++ primes.py
@@ -2,5 +2,23 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+  if number_of_primes <= 0:
+    raise ValueError("number_of_primes must be a positive integer")
+
+  primes = []
+  current = 2
+  while len(primes) < number_of_primes:
+    is_prime = True
+    for prime in primes:
+      if current % prime == 0:
+        is_prime = False
+        break
+
+    if is_prime:
+      primes.append(current)
+
+    current += 1
+
+  return primes
+
+print(primes(10))
